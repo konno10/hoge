b5758c56c654c910d1de279b1e31fe7ed7ab463c
53syo <53syo416baseball@gmail.com>
2022-05-19 18:52:07 +0900

    page3

--- src/views/Page3.vue
+++ src/views/Page3.vue
@@ -1,13 +1,34 @@
 <template>
-  <div>page3</div>
+  <div>
+    <datepicker :language="ja" v-model="newDate"  :clear-button="true" format="yyyy.MM.dd"/>
+    <div v-if="newDate">{{newDate|date}}が選択されました</div>
+  </div>
 </template>
 
 <script>
+import Datepicker from "vuejs-datepicker";
+import moment from "moment";
+ import { ja } from "vuejs-datepicker/dist/locale";
+
 export default {
+  components: {
+    Datepicker
+  },
+  computed: {
+    ja(){
+        return ja;
+    }
+  },
+  data(){
+    return{
+      newDate:"",
+    }
+  },
+  filters:{
+    date(newDate) {
+      return moment(newDate).format("y年M月D日");
+    }
+  }
 
-}
+};
 </script>
-
-<style>
-
-</style>
