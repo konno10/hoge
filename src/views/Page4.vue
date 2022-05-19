cc2456929cee6e78e78e6490d6ee099f5e6fe199
53syo <53syo416baseball@gmail.com>
2022-05-19 18:55:49 +0900

    page4

--- src/views/Page4.vue
+++ src/views/Page4.vue
@@ -1,58 +1,13 @@
 <template>
-  <div>
-    <input type="text" v-model="newTodo">
-    <button @click="onClicked">add</button>
-    <ul>
-      <li v-for="(hoge,index) in todos" :key="index">
-        <input type="checkbox" v-model="hoge.isDone">
-        <span :class="{done:hoge.isDone}">{{hoge.name}}</span>
-        <button @click="deleteTodo(hoge.name, index)">delete</button>
-      </li>
-    </ul>
-  </div>
+  <div>page4</div>
 </template>
 
 <script>
 export default {
-  data(){
-    return{
-      newTodo:"",
-      todos:[]
-    }
-  },
-  methods:{
-    onClicked(){
-      const newItem = {
-        name : this.newTodo,
-        isDone : false
-      }
-      this.todos.push(newItem)
-    },
-    deleteTodo(name, index) {
-      if (confirm(name+"を本当に削除しますか？")) {
-         this.todos.splice(index, 1);
-      }
-    },
-  }
 
 }
 </script>
 
-<style scoped>
-.done {
-    opacity: 0.5;
-    text-decoration: line-through;
-}
-li {
-    list-style: none;
-    margin-bottom: 10px;
-}
-span,
-input,
-button {
-    margin-right: 5px;
-}
-hr {
-    margin: 20px 0;
-}
+<style>
+
 </style>
