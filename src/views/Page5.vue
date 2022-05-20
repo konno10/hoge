3864d88dd6e0d5a1dc4922b613549980c8371bc2
53syo <53syo416baseball@gmail.com>
2022-05-20 15:57:22 +0900

    page5作成

--- src/views/Page5.vue
+++ src/views/Page5.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <router-link :to="month == 1 ? `/Page5/${year-1}/12` : `/Page5/${year}/${month-1}`"></router-link>
+    <router-link :to="month == 1 ? `/Page5/${year-1}/12` : `/Page5/${year}/${month-1}`">＜</router-link>
     <span>{{year}}年{{month}}月</span>
     <router-link :to="month ==12? `/page5/${year+1}/1` : `/page5/${year}/${month+1}`">＞</router-link>
   </div>
